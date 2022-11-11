36e02cb1a8b021dada07dfe39c843ebc7f8bca1f
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2022-11-11 15:09:28 -0500

    ManyToOne
    union entre Descripcion a a Pelicula

--- Cine/src/com/tuempresa/cine/modelo/Descripcion.java
+++ Cine/src/com/tuempresa/cine/modelo/Descripcion.java
@@ -3,17 +3,9 @@ package com.tuempresa.cine.modelo;
 import javax.persistence.*;
 
 import lombok.*;
-
 @Entity @Getter @Setter
-public class Descripcion {
-
-	@Column(length = 32)
-	String genero;
+public class Descripcion extends Identificable{
 	
 	@Column(length = 32)
 	String descripcion;
-	
-	@Column(length = 32)
-	String tipo;
-	
 }
